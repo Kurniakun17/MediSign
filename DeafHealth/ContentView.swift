40d0f2619b166c3a85a88faba6d85957b14951c3
Kurniakun17 <kurniakharisma17@gmail.com>
2024-08-13 12:32:58 +0700

    Initial Commit

---
+++ DeafHealth/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DeafHealth
+//
+//  Created by Kurnia Kharisma Agung Samiadjie on 13/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DeafHealth/DeafHealthApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeafHealthApp.swift
+//  DeafHealth
+//
+//  Created by Kurnia Kharisma Agung Samiadjie on 13/08/24.
+//
+
+import SwiftUI
+
+@main
+struct DeafHealthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
