31a7c590fb4105c9ee2a0e57759e25e5452d49a4
alifiyahariandri <alifiyah.nur@ui.ac.id>
2024-08-18 19:16:34 +0700

    feat(consultation): add start time view

--- DeafHealth/View/SymptomStartTimeView.swift
+++ DeafHealth/View/SymptomStartTimeView.swift
@@ -11,6 +11,13 @@ struct SymptomStartTimeView: View {
     @EnvironmentObject var coordinator: Coordinator
     @Environment(\.dismiss) private var dismiss
 
+    @State var viewModel: SymptomStartTimeViewModel = .init()
+
+    @State var isLainnyaSelected = false
+
+    @State var selectedTime: String = "_____"
+    @State var selectedDate: Date = .init()
+
     var body: some View {
         VStack(spacing: 0) {
             ZStack {
@@ -21,14 +28,14 @@ struct SymptomStartTimeView: View {
                 HStack {
                     // Custom back button
                     Button(action: {
-                        coordinator.pop()  // Handle back navigation
+                        coordinator.pop() // Handle back navigation
                     }) {
                         Image(systemName: "chevron.left")
-                            .foregroundColor(Color("black"))  // Replace with your color
+                            .foregroundColor(Color("black")) // Replace with your color
                     }
                     .padding(.leading)
 
-                    Spacer()  // This spacer helps to align the progress bar centrally
+                    Spacer() // This spacer helps to align the progress bar centrally
                 }
             }
             .padding(.top, 16)
@@ -39,6 +46,50 @@ struct SymptomStartTimeView: View {
                     .font(.title3)
                     .multilineTextAlignment(.center)
             }
+
+            ForEach(viewModel.options) { option in
+                VStack {
+                    ZStack {
+                        Text(option.title).font(Font.custom("SF Pro", size: 16)
+                            .weight(.medium))
+                            .foregroundColor(option.isSelected ? .white : .black)
+                    }
+                    .padding(.horizontal, 17)
+                    .padding(.vertical, 6)
+                    .background(option.isSelected ? Color(red: 0.65, green: 0.76, blue: 0.64) : Color(red: 0.95, green: 0.95, blue: 0.95).opacity(0.95))
+                    .cornerRadius(5)
+                    .onTapGesture {
+                        viewModel.selectedOption(optionId: option.id)
+                        selectedTime = option.title.lowercased()
+                        isLainnyaSelected = false
+                    }
+                }.padding(.vertical, 3)
+            }
+
+            HStack {
+                ZStack {
+                    Text("+ Lainnya").font(Font.custom("SF Pro", size: 16)
+                        .weight(.medium))
+                        .foregroundColor(isLainnyaSelected ? .white : .black)
+                }
+                .padding(.horizontal, 17)
+                .padding(.vertical, 6)
+                .background(isLainnyaSelected ? Color(red: 0.65, green: 0.76, blue: 0.64) : Color(red: 0.95, green: 0.95, blue: 0.95).opacity(0.95))
+                .cornerRadius(5)
+                .onTapGesture {
+                    viewModel.selectedOption()
+                    isLainnyaSelected = true
+                }
+
+                if isLainnyaSelected {
+                    DatePicker("", selection: $selectedDate, displayedComponents: .date)
+                        .datePickerStyle(.compact)
+                        .onChange(of: selectedDate) { newDate in
+                            selectedTime = DateFormatter.localizedString(from: newDate, dateStyle: .medium, timeStyle: .none)
+                        }
+                }
+            }
+
             Spacer()
 
             // Green overlay at the bottom with buttons
@@ -50,11 +101,15 @@ struct SymptomStartTimeView: View {
                     .padding(.leading, 32)
                     .frame(maxWidth: .infinity, alignment: .leading)
 
-                Text("Halo Dokter. Saya merasakan gejala ini sejak _____.")
-                    .font(.subheadline)
-                    .padding(.bottom, 8)
-                    .padding(.leading, 32)
-                    .frame(maxWidth: .infinity, alignment: .leading)
+                HStack {
+                    Text("Halo Dokter. Saya merasakan gejala ini sejak ")
+                        + Text("\(selectedTime)").bold()
+                        + Text(".")
+                }
+                .font(.subheadline)
+                .padding(.bottom, 8)
+                .padding(.leading, 32)
+                .frame(maxWidth: .infinity, alignment: .leading)
 
                 HStack(spacing: 16) {
                     Button("Kembali") {
